f2a0d929c79170e224674fb59c7ad9c897bd936d
Anastasiya Zhurava-Skarabahataya <59134438+Zhurava-Skarabahataya@users.noreply.github.com>
2023-02-26 14:57:55 +0300

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
-streamlit.title("Everything is good!")
+streamlit.title("New health cafe!")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🫐🍵 Omega 3 & Blueberry Oatmeal')
+streamlit.text('💥 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🪺 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocade toats!')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
